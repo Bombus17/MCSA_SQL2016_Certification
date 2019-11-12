a891cd4716269904d8097673f014c21c9fb1004a
azmathias <azramathias@stuperuser.onmicrosoft.com>
2019-11-12 13:24:07 +0000

    Amend Sub Queries

--- 07_Subqueries/Subqueries.sql
+++ 07_Subqueries/Subqueries.sql
@@ -26,16 +26,21 @@ FROM Production.Product
 WHERE ProductID IN (SELECT ProductID FROM Sales.SalesOrderDetail);
 
 /* Products that have yet to be ordered
+-- and are not display/0 price products
 ---------------------------------------*/
 SELECT ProductID, [Name] AS ProductName
-, Color
+, ISNULL(Color, '')AS Colour
 , ListPrice
 FROM Production.Product
 WHERE ProductID NOT IN (SELECT ProductID 
 						FROM Sales.SalesOrderDetail
-						WHERE ProductID IS NOT NULL);
+						WHERE ProductID IS NOT NULL)
+AND ListPrice > 0
+ORDER BY ProductID;
 
 /* scalar subquery 
+-- highest value order 
+-- cheapest (non display) product
 ---------------------------------------------------------------------*/
 SELECT SalesOrderID, TotalDue
 FROM Sales.SalesOrderHeader
@@ -43,7 +48,8 @@ WHERE TotalDue = (SELECT MAX(TotalDue) FROM Sales.SalesOrderHeader);
 
 SELECT productid, [Name], ListPrice
 FROM Production.Product
-WHERE ListPrice = (SELECT MIN(ListPrice) FROM Production.Product);
+WHERE ListPrice = (SELECT MIN(ListPrice) FROM Production.Product
+					WHERE ListPrice != 0);
 
 
 /* MULTI VALUE SUB QUERY 
@@ -60,7 +66,7 @@ WHERE Person.BusinessEntityID IN
 	 WHERE Year(BirthDate)>1980)
 ORDER BY LastName, FirstName;
 
--- products supplied by suppliers from Japan
+-- all products listed as a bike
 SELECT productid, [Name], listPrice 
 FROM Production.Product
 WHERE ProductSubcategoryID IN
@@ -81,7 +87,7 @@ GROUP BY OrderYear
 ORDER BY OrderYear;
 
 /* CORRELATED SUBQUERY 
-
+-- Sales Assistants with high sales
 ----------------------------------------------*/
 -- FIRST QUERY
 SELECT DISTINCT p.BusinessEntityID
@@ -99,7 +105,7 @@ WHERE s.TotalDue>110000;
 SELECT p.BusinessEntityID, FirstName + ' ' + LastName Salesperson 
 FROM Person.Person p
 WHERE EXISTS (SELECT s.SalesPersonID FROM Sales.Salesorderheader s -- this query cannot be run independently and is therefore correlated to the outer query
-				WHERE TotalDue>150000
+				WHERE TotalDue>110000
 				and p.BusinessEntityID=s.SalesPersonID);
 
 
@@ -113,9 +119,9 @@ WHERE ListPrice =
 
 /* use correlated sub query to find the nth highest salary
 	for each record processed by the outer query the inner query returns 
-	how many records has a value less than the value stated (2 in this example)
+	how many records has a value less than the value stated 
 ----------------------------------------------------------------------------*/
-DECLARE @Nth INT = 2
+DECLARE @Nth INT = 4
 
 SELECT CASE @Nth
 		WHEN 2 THEN 'Second Highest salary'
@@ -136,7 +142,9 @@ WHERE @Nth=(SELECT COUNT(DISTINCT Salary)
 -- alternative solution for products with minimum price
 SELECT productid, [Name], ListPrice
 FROM Production.Product
-WHERE ListPrice <= ALL (SELECT ListPrice FROM Production.Product);
+WHERE ListPrice <= ALL (SELECT ListPrice FROM Production.Product
+						WHERE ListPrice > 0)
+AND ListPrice > 0;
 
 -- ANY / SOME
 -- products with price that is not the minimum
@@ -145,20 +153,21 @@ FROM Production.Product
 WHERE ListPrice > ANY (SELECT ListPrice FROM Production.Product);
 
 /* EXIST and NOT EXISTS in sub queries
-
+-- note think about INTERSECT and EXCEPT here too. 
+-- review those queries and look at performance
 -----------------------------------------------*/
--- customers who placed an order on February 18, 2013
+-- customers who placed an order on February 18, 2012
 SELECT CustomerID, AccountNumber
 FROM Sales.Customer AS C
 WHERE EXISTS (SELECT *
 			FROM Sales.SalesOrderHeader AS O
 			WHERE O.CustomerID = C.CustomerID
-			AND O.orderdate = '20130218');
+			AND O.orderdate = '20120218');
 
--- customers who did not place an order on February 18, 2013
+-- customers who did not place an order on February 18, 2012
 SELECT CustomerID, AccountNumber
 FROM Sales.Customer AS C
 WHERE NOT EXISTS (SELECT *
 					FROM Sales.SalesOrderHeader AS O
 					WHERE O.CustomerID = C.CustomerID
-					AND O.orderdate = '20130218');
+					AND O.orderdate = '20120218');
